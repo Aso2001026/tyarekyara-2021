da3e31ab2030f9bfac852c8911fdd742d0267c8c
Aso2001026 <83051005+Aso2001026@users.noreply.github.com>
2022-06-07 09:20:33 +0900

    Update googleout.php

--- 本番1/ANYPORT1/googleout/googleout.php
+++ 本番1/ANYPORT1/googleout/googleout.php
@@ -7,7 +7,7 @@ require './header.php';
 <div class="googleMap">
     <p>目的地周辺を表示します</p>
     <?php
-    echo '<a href="https://www.google.com/maps/dir/?api=AIzaSyD756fOGZoQkH4WL5B266Ivy8h6GtSDsIU&destination=',$_POST['mokuteki'],'">mapへ移動</a>';
+    echo '<a href="https://www.google.com/maps/dir/?api=APIkey&destination=',$_POST['mokuteki'],'">mapへ移動</a>';
     ?>
 </div>
 <?php require './footer.php'; ?>
